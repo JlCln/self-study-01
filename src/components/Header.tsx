e1454a114173f7aa935d1d38eddf9df74664083d
JlCln <j.coulon.pro@gmail.com>
2024-10-10 18:54:15 +0200

    modified imgs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,11 @@ function Header() {
         <div className="header-container">
           <h1 className="title-text">Les défis et les avantages du développement front-end</h1>
           <div className="header-logos">
-          <img src="assets/logo-html5.svg" className="header-logo" id="html-logo"></img>
-          <img src="assets/logo-css3.svg" className="header-logo" id="css-logo"></img>
-          <img src="assets/logo-javascript.svg" className="header-logo" id="js-logo"></img>
-          <img src="assets/git-branch-outline.svg" className="header-logo" id="git-logo"></img>
-          <img src="assets/logo-react.svg" className="header-logo" id="react-logo" ></img>
+          <img src="public/logo-html5.svg" className="header-logo" id="html-logo"></img>
+          <img src="public/logo-css3.svg" className="header-logo" id="css-logo"></img>
+          <img src="public/logo-javascript.svg" className="header-logo" id="js-logo"></img>
+          <img src="public/git-branch-outline.svg" className="header-logo" id="git-logo"></img>
+          <img src="public/logo-react.svg" className="header-logo" id="react-logo" ></img>
           </div>
 
         </div>
